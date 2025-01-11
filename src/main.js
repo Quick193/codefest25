778ae36ff6598b4c63ff95ff8578d1d5ec9b1a8c
Quick193 <makenavishal01@gmail.com>
2025-01-11 11:17:46 +0530

    deleted:    Quick_Oakfest25 	modified:   src/main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './main.css';
 
 const Main = () => {
     return (
-        <div className="body">
+        <div className="main-container">
             <div className="title">
                 <h1>Placeholder</h1>
             </div>
